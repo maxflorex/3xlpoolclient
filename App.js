c40fc876c4cfdd6ed037fb7f3e81f4135bdf8512
Max Flores <max@maxflores.dev>
2022-10-21 22:20:35 -0600

    Tab Menu

--- App.js
+++ App.js
@@ -3,19 +3,26 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen';
 import PoolFormScreen from './src/screens/PoolFormScreen';
-import IntroScreen from './src/screens/IntroScreen';
 import { COLORS } from './src/components/Colors';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Stack = createStackNavigator()
+
+// ! SCREEN NAMES
+const homeScreen = 'Home'
+const user = 'User'
+
+const Tab = createBottomTabNavigator()
+
 
 export default function App() {
 
 	return (
 		<NavigationContainer>
-			<Stack.Navigator>
-				{/* HOME */}
+			{/* <Stack.Navigator> */}
+			{/* HOME */}
 
-				<Stack.Screen
+			{/* <Stack.Screen
 					name='Home'
 					component={HomeScreen}
 					options={({ navigation }) => ({
@@ -31,29 +38,11 @@ export default function App() {
 							</TouchableOpacity>
 						)
 					})}
-				/>
-
-				{/* <Stack.Screen
-					name='Home'
-					component={HomeScreen}
-					options={({ navigation }) => ({
-						title: '3XL Pool',
-						headerStyle: { backgroundColor: `${COLORS.light}` },
-						headerTitleStyle: { color: `${COLORS.dark}` },
-
-						// ADD NEW BUTTON
-
-						headerRight: () => (
-							<TouchableOpacity onPress={() => navigation.navigate('PoolFormScreen')}>
-								<Text style={{ color: '#ffffff', marginRight: 20, fontSize: 15 }}>Pool</Text>
-							</TouchableOpacity>
-						)
-					})}
 				/> */}
 
-				{/* POOL */}
+			{/* POOL */}
 
-				<Stack.Screen name='PoolFormScreen'
+			{/* <Stack.Screen name='PoolFormScreen'
 					component={PoolFormScreen}
 					options={{
 						title: 'Pool',
@@ -63,8 +52,17 @@ export default function App() {
 						headerTitleStyle: { color: '#ffffff' },
 						headerTintColor: '#3BBA9C'
 					}}
-				/>
-			</Stack.Navigator>
+				/> */}
+			{/* </Stack.Navigator> */}
+		<Tab.Navigator
+		initialRouteName={homeScreen}
+		screenOptions={({route}) => ({
+			// tabBar
+		})} 
+		>
+
+		</Tab.Navigator>
+		
 		</NavigationContainer>
 	);
 }
